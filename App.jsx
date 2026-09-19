6d3da424bc285f15b6e2da5724fbfbc2dd9b227c
Sakshi Singh <singhsakshii1708@gmail.com>
2026-09-19 11:31:31 +0530

    Update import paths for components in App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import PersonalInfo from "./components/PersonalInfo";
-import AccountDetails from "./components/AccountDetails";
-import Review from "./components/Review"; 
+import PersonalInfo from "/PersonalInfo";
+import AccountDetails from "/AccountDetails";
+import Review from "/Review"; 
 
 
 function App() {
